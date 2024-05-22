be21908948d7933b8efe3040d49d7cccad46bcdb
rahathasanDev <devrahat.hasan@gmail.com>
2024-05-22 11:10:37 +0600

    Fetch Dynamic Data Using GenerateStaticParams() For SSG , and one bugs fixed which is genrateStaticParams function should be this name

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,27 +1,32 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
 
-interface BlogId{
-  params:{
-    blogId:string;
+interface BlogId {
+  params: {
+    blogId: string;
   };
-
 }
-const BlogDetailPage = async({params}:BlogId) => {
+
+export const generateStaticParams = async() => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0,3).map((blog:Blog) => ({
+    blogId : blog.id  ,
+  }));
+  // only one semicolon is enough for you 
+
+};
+const BlogDetailPage = async ({ params }: BlogId) => {
   // console.log(params);
-  
-  const res = await fetch (`http://localhost:5000/blogs/${params.blogId}`,
-    {
-      cache:"no-store"
-    }
-  );
+
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
   const blog = await res.json();
-  
-
-
 
   return (
     <div className="m-10">
-      <BlogDetails blog={blog}/>
+      <BlogDetails blog={blog} />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
+import Banner from "@/components/shared/Banner";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/blogs",{
-    next : {
-      revalidate:30,
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: {
+      revalidate: 30,
     },
-  })
+  });
   const blogs = await res.json();
   // console.log(blogs);
   return (
     <>
+      <Banner />
       <LatestBlogs blogs={blogs}></LatestBlogs>{" "}
     </>
   );
